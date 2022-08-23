daaff4b47c06cb42237cec3d39e425463199e119
MiersX <michael.muirhead123@gmail.com>
2022-08-23 04:42:36 +0000

    Minor syntax updates

--- head.js
+++ head.js
@@ -3,13 +3,7 @@ const assertEqual = function(actual, expected) {
     : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-
-
 const head = function(array) {
   let firstElement = array[0];
   return firstElement;
 };
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
