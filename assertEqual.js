f03cbea1431af278f48bb93eab72b00c34196937
MiersX <michael.muirhead123@gmail.com>
2022-08-23 02:56:53 +0000

    Function that compares values and outputs a highlighted success or failure message

---
+++ assertEqual.js
@@ -0,0 +1,26 @@
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+/*
+const assertEqual = function (actual, expected) {
+  actual === expected
+    ? console.log("Assertion Passed: " + actual + " === " + expected)
+    : console.log("Assertion Failed: " + actual + " !== " + expected);
+};
+*/
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Descartes", "Descartes");
+assertEqual("Hobbes", "Foucault");
+assertEqual(5, 5);
+assertEqual(25, 31);
