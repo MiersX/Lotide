fac6e4697b529e2f727be6fec7bea2d3ece2f20b
MiersX <michael.muirhead123@gmail.com>
2022-08-23 03:37:21 +0000

    Refactored with template literals and ternary operator

--- assertEqual.js
+++ assertEqual.js
@@ -1,22 +1,18 @@
-
-
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-  }
+  actual === expected ? console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
+
 
 /*
-const assertEqual = function (actual, expected) {
-  actual === expected
-    ? console.log("Assertion Passed: " + actual + " === " + expected)
-    : console.log("Assertion Failed: " + actual + " !== " + expected);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 */
-
-
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
