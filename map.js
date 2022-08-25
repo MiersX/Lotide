a1fe68e77445dfcc6c989e1a8426c61461a38939
MiersX <michael.muirhead123@gmail.com>
2022-08-25 21:48:24 +0000

    Recreating the .map method from its building blocks

---
+++ map.js
@@ -0,0 +1,44 @@
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
+  }
+  if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+
+const map = (array, callback) => {
+  const newArr = [];
+
+  for (const element of array) {
+    newArr.push(callback(element));
+  }
+  return newArr;
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
