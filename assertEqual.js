49d4e9e79d3a91238e3d54e0b79a149a32c622f2
MiersX <michael.muirhead123@gmail.com>
2022-08-29 20:37:39 +0000

    Moved the testing components into a separate file

--- assertEqual.js
+++ assertEqual.js
@@ -4,21 +4,5 @@ const assertEqual = function(actual, expected) {
 };
 
 
-/*
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-*/
-/*
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Descartes", "Descartes");
-assertEqual("Hobbes", "Foucault");
-assertEqual(5, 5);
-assertEqual(25, 31);
-*/
 
+module.exports = assertEqual;
