aed55158a8c709d4a6584f67c413a690ff5b3d66
MiersX <michael.muirhead123@gmail.com>
2022-08-29 20:55:26 +0000

    Updated to export the functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -22,9 +22,11 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
+/*
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 
+*/
+module.exports = [eqArrays, assertArraysEqual]
